4e3c1078d698baf50a4edaa8f7ce4d1804dfecc5
unknown <h.th2828@gmail.com>
2022-06-14 17:21:45 +0200

    first commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,22 @@
-import logo from './logo.svg';
-import './App.css';
-
+import "./App.css";
+import HomePage from "./pages/HomePage";
+import Testimonials from "./pages/Testimonials"
+import {
+  BrowserRouter as Router,
+  Switch,
+  Route,
+  Routes,
+} from "react-router-dom";
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      
+      <Routes>
+         <Route path="/" element={<HomePage/>} /> 
+        <Route path="/testimonials" element={<Testimonials/>} />
+
+
+      </Routes>
     </div>
   );
 }
---
+++ src/theme/theme.js
@@ -0,0 +1,19 @@
+import { createTheme } from '@mui/material/styles';
+
+const theme = createTheme({
+    "palette": {
+       
+        "primary": {
+            "main": "#FFFFFF",
+        },
+        "secondary": {
+            "main": "#23D4CE",
+        },
+        "info": {
+            "main": "#000000",
+        },
+              },
+    
+});
+
+export default theme;
